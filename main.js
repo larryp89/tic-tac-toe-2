b0f063ecc810684bffc60e8fcf3651a78676372c
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-08-26 17:59:59 +0100

    Completed game logic, finalising UI ineraction and UI

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
+const player = function (name, marker) {
+  const playerName = name;
+  const playerMarker = marker;
+  let score = 0;
+  const getScore = () => score;
+  const addWin = function () {
+    score += 1;
+  };
+
+  return { playerName, playerMarker, addWin, getScore };
+};
+
 const gameBoard = (function () {
-  const grid = ["O", "X", "O", "", "", "", "", "", ""];
+  const grid = ["", "", "", "", "", "", "", "", ""];
+  let playerOne;
+  let playerTwo;
+  let currentPlayer;
+
+  const resetBoard = function () {
+    for (let i = 0; i < grid.length; i++) {
+      grid[i] = "";
+    }
+  };
+
+  const initGame = function (playerOneName, playerTwoName) {
+    playerOne = player(playerOneName, "X");
+    playerTwo = player(playerTwoName, "O");
+    currentPlayer = playerOne;
+  };
 
   const updateGrid = function (index, value) {
     grid[index] = value;
@@ -23,24 +50,102 @@ const gameBoard = (function () {
         return true;
       }
     }
-
-    return false;
   };
 
-  const checkValidMove = function (move) {
-    if (grid[move] === "") {
-      updateGrid(move);
-    } else {
-      return "FAIL";
+  const checkDraw = function () {
+    return grid.every(function (cell) {
+      return cell !== "";
+    });
+  };
+
+  const checkValidMove = function (index) {
+    if (grid[index] === "") {
+      return true;
     }
   };
 
-  return { grid, updateGrid, checkWinner, checkValidMove };
+  const getCurrentPlayer = () => currentPlayer;
+
+  const changeCurrentPlayer = function () {
+    currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+  };
+
+  return {
+    grid,
+    updateGrid,
+    checkWinner,
+    checkDraw,
+    checkValidMove,
+    getCurrentPlayer,
+    changeCurrentPlayer,
+    initGame,
+    resetBoard,
+  };
 })();
 
-const player = function (name, marker) {
-  const playerName = name;
-  const playerMarker = marker;
+const UIManager = function () {
+  const dialog = document.querySelector("dialog");
+  // show modal
+  const showModal = () => dialog.showModal();
+  const hideModal = () => dialog.close();
 
-  return { playerName, playerMarker };
+  // get form inputs
+  const submitButton = document
+    .querySelector("#submit")
+    .addEventListener("click", function () {
+      const playerOneName = document.querySelector("#player-one").value;
+      const playerTwoName = document.querySelector("#player-two").value;
+      gameBoard.initGame(playerOneName, playerTwoName);
+      hideModal();
+      const mainWrapper = document.querySelector(".main-wrapper");
+      mainWrapper.classList.toggle("hidden");
+    });
+
+  // get DOM element, add event listener and game-flow
+  const mainGrid = document.querySelector(".main-grid");
+
+  const updateGrid = function (div, marker) {
+    div.textContent = marker;
+  };
+
+  const resetBoard = function () {
+    allDivs = document.querySelectorAll(".main-grid>div");
+    for (let div of allDivs) {
+      div.textContent = "";
+    }
+  };
+
+  mainGrid.addEventListener("click", function (event) {
+    // get the current player
+    let currentPlayer = gameBoard.getCurrentPlayer();
+    // identify the clicked div
+    let clickedDiv = event.target;
+    // get index of the div clicked to update the grid array
+    let divIndex = parseInt(clickedDiv.getAttribute("data-index"));
+    // get marker of the current player
+    marker = currentPlayer.playerMarker;
+
+    // if it's a valid move
+    if (gameBoard.checkValidMove(divIndex)) {
+      // update the array grid and UI grid
+      gameBoard.updateGrid(divIndex, marker);
+      updateGrid(clickedDiv, marker);
+      // check if there is a winner
+      if (gameBoard.checkWinner() || gameBoard.checkDraw()) {
+        // show modal with playAgain or quit
+        // if playAgain, update the scores and reset board and UI
+      }
+      // Otherwise
+      gameBoard.changeCurrentPlayer();
+    } else {
+      alert("Invalid move");
+    }
+  });
+
+  return { resetBoard, showModal };
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  UIInstance = UIManager();
+  UIInstance.showModal();
+});
